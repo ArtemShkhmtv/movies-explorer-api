18d2e039f1b53639eb5ee64f42757436e41671d3
Artem Shkhmtv <artem.uralovich@yandex.ru>
2023-08-24 23:57:58 +0300

    превентивный корс

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:3001',
-    'https://art.nomoreparties.co',
+    'https://artpr.nomoredomainsicu.ru',
   ],
   credentials: true,
 }));
